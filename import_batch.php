ba7cb26b8dab3d84483db33b2e04a8d8ce4e56a3
Daljit Katoch <djkatoch@gmail.com>
2023-05-20 11:23:14 +0530

    Update import_batch.php

--- import_batch.php
+++ import_batch.php
@@ -15,29 +15,28 @@ if ($conn->connect_error) {
 /* get total record */
 $query_count = "SELECT count(*) as total_records FROM tablename";
 $result_num = $conn->query($query_count);
-$records = $result_num->fetch_assoc(); 
+$records = $result_num->num_rows; 
 
 $batchSize = 10000; /* Batch Size*/
-$totalRecords = $records['total_records']; /* Total number of records to import */
+$totalRecords = $records; /* Total number of records to import */
 $offset = 0; /* Starting offset for each batch */
 
 while ($offset < $totalRecords) {
 	
     /* Fetch a batch of record */
-	$query_batch = "SELECT * FROM tablename LIMIT $offset, $batchSize";
+    $query_batch = "SELECT * FROM tablename LIMIT $offset, $batchSize";
     $result = $conn->query($query_batch);
 
     if ($result->num_rows > 0) {        
         while ($row = $result->fetch_assoc()) {
-		         /* write a code to import */
-            echo $row['column_name'];
-			      ob_flush();	
-			      flush();
-			      usleep(100000);
+		/* write a code to import */
+           	echo $row['column_name'];
+		ob_flush();	
+		flush();
+		usleep(100000);
         }        
     }
-	  $offset += $batchSize;
-    $result->free_result();
+    $offset += $batchSize;
 
 }
 
